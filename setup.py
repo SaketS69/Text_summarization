6df518d45993b15b0908ab6909bc6d6b373cfdc9
SaketS69 <saket7893@gmail.com>
2024-12-01 22:54:10 +0530

    First Commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Text-Summarization-Pytorch",
+    version="0.0.1",
+    author="Saket Suman",
+    author_email="sumansaket197@gmail.com",
+    packages=find_packages(),
+)
